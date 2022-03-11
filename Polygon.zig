bb9b43ce1f9f8ef7235ecbd7a12431236798dce7
Silver <silver@silv.re>
2022-03-11 09:10:35 +0000

    Optimize polygon support function using hill climbing

--- Polygon.zig
+++ Polygon.zig
@@ -19,15 +19,50 @@ pub fn init(offset: v.Vec2, verts: []const v.Vec2) Polygon {
 
 /// Returns the furthest vertex in direction d
 pub fn support(self: Polygon, d: v.Vec2) v.Vec2 {
-    var max_dot = -std.math.inf(f64);
-    var max_v: v.Vec2 = undefined;
-    // TODO: binary search
-    for (self.verts) |vert| {
-        const dot = v.dot(d, vert);
-        if (dot > max_dot) {
-            max_dot = dot;
-            max_v = vert;
+    // Start by sampling a few points
+    var supp = Support.init(self, 0, d);
+    _ = supp.improve(self, self.verts.len / 4, d);
+    _ = supp.improve(self, self.verts.len / 2, d);
+    _ = supp.improve(self, 3 * self.verts.len / 4, d);
+
+    // Improve the guess
+    while (supp.climb(self, d)) {}
+
+    return self.verts[supp.idx] + self.offset;
+}
+const Support = struct {
+    idx: usize,
+    dot: f64,
+
+    fn init(self: Polygon, idx: usize, d: v.Vec2) Support {
+        return .{
+            .idx = idx,
+            .dot = v.dot(d, self.verts[idx]),
+        };
+    }
+
+    fn improve(supp: *Support, self: Polygon, idx: usize, d: v.Vec2) bool {
+        const new = Support.init(self, idx, d);
+        if (new.dot > supp.dot) {
+            supp.* = new;
+            return true;
+        } else {
+            return false;
         }
     }
-    return max_v + self.offset;
-}
+
+    fn climb(supp: *Support, self: Polygon, d: v.Vec2) bool {
+        const li = if (supp.idx + 1 >= self.verts.len)
+            0
+        else
+            supp.idx + 1;
+        const ri = if (supp.idx == 0)
+            self.verts.len - 1
+        else
+            supp.idx - 1;
+
+        const l_good = supp.improve(self, li, d);
+        const r_good = supp.improve(self, ri, d);
+        return l_good or r_good;
+    }
+};
